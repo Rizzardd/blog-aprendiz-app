b3cbfb6b1b263f96c098c52ca0f3d9cdbace1a38
Rizzardd <87292423+Rizzardd@users.noreply.github.com>
2022-10-27 00:19:56 -0400

    files

---
+++ src/Article.js
@@ -0,0 +1,79 @@
+import React from 'react'
+
+
+
+function Article(){
+  return (
+    <>
+      <div id="articleOne" className="content-column">
+          <div className="accordion-item">
+            <h2 className="accordion-header" id="headingOne">
+              <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne"
+                aria-expanded="false" aria-controls="collapseOne">
+                <div className="content-item">
+                  <img
+                    src="https://s2.glbimg.com/kWEvA_AIcdA564rsiakOziMpgqE=/0x0:1651x1100/1008x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2018/B/Z/nnHObjRbS6mYfBLJd4Bw/pet1.jpg"
+                    alt="porquinho da india com brócolis" />
+                    <p className="content-item-title">
+                      O que porquinho da índia pode comer?
+                    </p>
+                </div>
+              </button>
+            </h2>
+            <div id="collapseOne" className="accordion-collapse collapse" aria-labelledby="headingOne"
+              data-bs-parent="#articleOne">
+              <div className="accordion-body">
+                Sabe o que porquinho da índia pode comer? Ainda que seja um roedor, esse animal carrega uma característica peculiar: ele é herbívoro. Ou seja, o tutor precisa saber exatamente o que oferecer ao seu animal de estimação para evitar quaisquer apuros. 
+                <br/>
+                Venha saber como seria uma alimentação saudável desse animal e quais os alimentos ele pode comer, continue a leitura! 
+                <br/>
+                <br/>
+                <strong>Então, o que porquinho da índia pode comer?</strong>
+                <br/>
+                Como o porquinho da índia é um roedor herbívoro, ele tem uma alimentação bem específica para não interferir na sua saúde. Como parte de sua dieta balanceada, ele costuma comer feno com muita frequência e uma ração voltada somente para a sua espécie. 
+                <br/>
+                Além disso, frutas, legumes e verduras são o que porquinho da índia pode comer, alimentos que oferecem nutrientes e vitaminas essenciais para o seu bem-estar. 
+                <br/>
+                Diferente do que se pode imaginar, a alimentação do porquinho da índia não é a mesma se comparada com outros roedores, como hamster e twister. Por se tratar de um herbívoro, ele não pode comer carne, ovo, laticínios ou insetos. E descarte os alimentos processados e com conservantes, ok? Nada que contenha açúcar, sal ou óleo pode ser oferecido ao pet. 
+                <br/>
+                <br/>
+                <strong>Como dar uma alimentação saudável para o porquinho da índia?</strong>
+                <br/>
+                Como dito, o porquinho da índia necessita de uma dieta equilibrada e saudável, uma vez que é um animal pequeno com um organismo sensível. Desta forma, o tutor precisa seguir à risca em oferecer os alimentos permitidos e respeitar a frequência recomendada. 
+                <br/>
+                Com relação aos alimentos, o feno é o mais consumido pelo roedor. Rico em fibras e importante para o bom funcionamento do intestino do pet, o feno deve estar disponível 24 horas por dia e fresco. Prefira o feno de capim, já que o de alfafa deve ser mais controlado por ter uma alta quantidade de cálcio. 
+                <br/>
+                O feno, inclusive, ajuda no desgaste dos dentes do animal, que crescem regularmente. Como estão sempre mastigando, o feno tem a função de impedir problemas na dentição. 
+                <br/>
+                Já sobre a ração, saiba que existe uma ração própria ao porquinho da índia. Este roedor necessita de uma ração diária que seja rica em vitamina C, propriedade que o animal não produz e precisa ingeri-la, igual aos humanos. 
+                <br/>
+                Sem a vitamina C, ele fica exposto a doenças, como o escorbuto. Ou melhor, é fundamental o pet consumir esse nutriente pensando em sua saúde. 
+                <br/>
+                As frutas e legumes devem ser oferecidas de forma mais moderada e diversificada. Assim, o animal consegue aproveitar as fibras, minerais e vitaminas dos alimentos. 
+                <br/>
+                Para as frutas, disponibilize duas vezes por semana ao roedor, em contrapartida os vegetais com folhas podem ser oferecidos diariamente. O restante dos legumes e verduras deixe em menor frequência na gaiola, como três vezes na semana. 
+                <br/>
+                Sempre frescos e lavados, ok? Não esqueça de retirar a sujeira que permanecer na gaiola, o que torna os alimentos impróprios ao consumo do pet. 
+                <br/>
+                Agora vamos conhecer os alimentos que o porquinho da índia pode comer.
+                <br/>
+                <br/>
+                <strong>Melhores alimentos que o porquinho da índia pode comer</strong>
+                <br/>
+                Para ficar mais claro ao tutor, fizemos uma lista dos alimentos com o que porquinho da índia pode comer, confira:
+                <br/>
+                <ul>
+                  <li>água fresca;</li>
+                  <li>feno de capim;</li>
+                  <li>Frutas: banana, caqui, frutas vermelhas, goiaba, kiwi, maçã, mamão, manga, melancia, melão, morango, pêra, pêssego, tomate; </li>
+                  <li>ração;</li>
+                  <li>Vegetais: abóbora, agrião, almeirão, berinjela, beterraba com folhas, cenoura, chicória, chuchu, coentro, escarola, espinafre, jiló, rabanete, repolho, rúcula, salsinha, pimentão.</li>
+                </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+   </>
+  );   
+}
+  export default Article;
